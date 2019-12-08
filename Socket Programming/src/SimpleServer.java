4b6d0d5232e4a77c965417b539b4bc6e3050d5d9
aslantr <resulaslantr@gmail.com>
2019-12-08 00:22:52 -0600

    Android jars updated

---
+++ Socket Programming/src/SimpleServer.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = 8000;
+
+        ServerSocket server = new ServerSocket(portNumber);
+
+        System.out.println("Server started at port number = "+ portNumber);
+
+        Socket client = server.accept();
+
+        System.out.println("Client connected!");
+
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+
+        out.write("Welcome"+"\n");
+        out.write("what is your name ");
+        out.flush();
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        String name = in.readLine();
+
+        out.write("Welcome "+ name+"\n");
+
+        out.flush();
+
+    }
+}
