b5b68215939e6eb31283fcc3b8243a45d0eda6ef
aslantr <resulaslantr@gmail.com>
2019-12-08 10:34:45 -0600

    JSF started

---
+++ MultithreadingInJava/src/SimpleThread.java
@@ -0,0 +1,48 @@
+public class SimpleThread {
+    public static void main2(String[] args) {
+        for (int i = 0; i <5 ; i++) {
+            System.out.print("a");
+
+        }
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.print("c");
+
+        }
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.print("d");
+
+        }
+    }
+    public  class ChanrThread extends  Thread {
+
+        private  char c;
+
+        public  ChanrThread(char c){
+            this.c =c;
+        }
+        @Override
+        public void run(){
+            System.out.print(c);
+
+        }
+    }
+    public void runThreads(){
+        for (int i = 0; i <100 ; i++) {
+            Thread a = new ChanrThread('a');
+            Thread b = new ChanrThread('b');
+            Thread c = new ChanrThread('c');
+            a.run();
+            b.run();
+            c.run();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new SimpleThread().runThreads();
+    }
+}
+
+
